60218b4bce434a42658ee7814e0087dc5347dd21
Rebecca Di Matteo <beccadimatteoo@gmail.com>
2023-06-05 15:47:44 +0200

    Add Test suite for Change Request 2

--- src/testing/CR2_Test_Case.py
+++ src/testing/CR2_Test_Case.py
@@ -6,7 +6,7 @@ import unittest
 class TestSuiteCR2(unittest.TestCase):
   
  
- #function to test the correct representation of explainability of community smells ose
+ #function to test the correct representation of explainability of community smells OSE
  def test_case_1_CR_2_ose(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -16,7 +16,7 @@ class TestSuiteCR2(unittest.TestCase):
         self.assertEqual(explainability_cadocs_ose,message_example_OSE_explainability)
 
 
- #function to test the incorrect representation of explainability of community smells ose
+ #function to test the incorrect representation of explainability of community smells OSE
  def test_case_1_1_CR_2_ose(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -26,7 +26,7 @@ class TestSuiteCR2(unittest.TestCase):
         self.assertEqual(explainability_cadocs_ose,message_example_OSE_explainability)
           
 
- #function to test the correct representation of explainability of community smells bce
+ #function to test the correct representation of explainability of community smells BCE
  def test_case_2_CR_2_bce(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -35,7 +35,7 @@ class TestSuiteCR2(unittest.TestCase):
         message_example_BCE_explainability="From the analysis implemented on Explainability using the SHAP framework, we can deduce that the prediction of the Community Smells BCE, depends on the socio-technical metric ND,the active relationships between developers within the team. To solve the problem within the team, active relationships between developers should be increased."
         self.assertEqual(explainability_cadocs_bce,message_example_BCE_explainability)
         
- #function to test the incorrect representation of explainability of community smells bce
+ #function to test the incorrect representation of explainability of community smells BCE
  def test_case_2_1_CR_2_bce(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -47,7 +47,7 @@ class TestSuiteCR2(unittest.TestCase):
 
 
 
- #function to test the correct representation of explainability of community smells pde      
+ #function to test the correct representation of explainability of community smells PDE      
  def test_case_3_CR_2_pde(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -56,7 +56,7 @@ class TestSuiteCR2(unittest.TestCase):
         message_example_PDE_explainability="From the analysis performed on Explainability using the SHAP framework, we can deduce that the prediction of the Community Smells PDE, depends on the socio-technical metric NAD, the number of active days of a developer on a project. To solve the problem within the team, developers should work more on the project."
         self.assertEqual(explainability_cadocs_pde,message_example_PDE_explainability)
         
- #function to test the incorrect representation of explainability of community smells pde      
+ #function to test the incorrect representation of explainability of community smells PDE      
  def test_case_3_1_CR_2_pde(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -67,7 +67,7 @@ class TestSuiteCR2(unittest.TestCase):
 
 
 
-#function to test the correct representation of explainability of community smells sv     
+#function to test the correct representation of explainability of community smells SV     
  def test_case_4_CR_2_sv(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -77,7 +77,7 @@ class TestSuiteCR2(unittest.TestCase):
         self.assertEqual(explainability_cadocs_sv,message_example_SV_explainability)
         
 
-#function to test the incorrect representation of explainability of community smells sv     
+#function to test the incorrect representation of explainability of community smells SV     
  def test_case_4_1_CR_2_sv(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -87,7 +87,7 @@ class TestSuiteCR2(unittest.TestCase):
         self.assertEqual(explainability_cadocs_sv,message_example_SV_explainability)
 
 
- #function to test the correct representation of explainability of community smells os      
+ #function to test the correct representation of explainability of community smells OS      
  def test_case_5_CR_2_os(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -96,7 +96,7 @@ class TestSuiteCR2(unittest.TestCase):
         message_example_OS_explainability="From the analysis performed on Explainability using the SHAP framework, we can deduce that the prediction of Community Smells OS, depends on the socio-technical metric NC, the number of developers in the community. To solve the problem within the team, developers should increase the number of developers within the project."
         self.assertEqual(explainability_cadocs_os,message_example_OS_explainability)
         
-#function to test the incorrect representation of explainability of community smells os      
+#function to test the incorrect representation of explainability of community smells OS      
  def test_case_5_1_CR_2_os(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -107,7 +107,7 @@ class TestSuiteCR2(unittest.TestCase):
 
 
 
-#function to test the correct representation of explainability of community smells sd      
+#function to test the correct representation of explainability of community smells SD     
  def test_case_6_CR_2_sd(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -116,7 +116,7 @@ class TestSuiteCR2(unittest.TestCase):
         message_example_SD_explainability="From the analysis implemented on Explainability using the SHAP framework, we can deduce that the prediction of Community Smells SD, depends on the socio-technical metric PCD, the percentage of core developers. To solve the problem within the team the developers should increase the number of core developers within the project."
         self.assertEqual(explainability_cadocs_sd,message_example_SD_explainability)
         
-#function to test the incorrect representation of explainability of community smells sd      
+#function to test the incorrect representation of explainability of community smells SD      
  def test_case_6_1_CR_2_sd(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -128,7 +128,7 @@ class TestSuiteCR2(unittest.TestCase):
 
 
 
- #function to test the correct representation of explainability of community smells rs       
+ #function to test the correct representation of explainability of community smells RS       
  def test_case_7_CR_2_rs(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -137,7 +137,7 @@ class TestSuiteCR2(unittest.TestCase):
         message_example_RS_explainability="From the analysis implemented on Explainability using the SHAP framework, we can deduce that the prediction of Community Smells RS, depends on the socio-technical metric ND, the active relationships between developers within the team. To solve the problem within the team, active relationships between developers should be increased."
         self.assertEqual(explainability_cadocs_rs,message_example_RS_explainability)
         
-#function to test the incorrect representation of explainability of community smells rs       
+#function to test the incorrect representation of explainability of community smells RS       
  def test_case_7_1_CR_2_rs(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -148,7 +148,7 @@ class TestSuiteCR2(unittest.TestCase):
 
 
 
- #function to test the correct representation of explainability of community smells tf        
+ #function to test the correct representation of explainability of community smells TF        
  def test_case_8_CR_2_tf(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -158,7 +158,7 @@ class TestSuiteCR2(unittest.TestCase):
         self.assertEqual(explainability_cadocs_tf,message_example_TF_explainability)
 
 
-#function to test the incorrect representation of explainability of community smells tf        
+#function to test the incorrect representation of explainability of community smells TF        
  def test_case_8_1_CR_2_tf(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -168,7 +168,7 @@ class TestSuiteCR2(unittest.TestCase):
         self.assertEqual(explainability_cadocs_tf,message_example_TF_explainability)
 
 
- #function to test the correct representation of explainability of community smells ui       
+ #function to test the correct representation of explainability of community smells UI      
  def test_case_9_CR_2_ui(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -177,7 +177,7 @@ class TestSuiteCR2(unittest.TestCase):
         message_example_UI_explainability="From the analysis performed on Explainability using the SHAP framework, we can deduce that the prediction of the Community Smells UI, depends on the socio-technical metric NCD, the number of commits per developer in a project. To solve the problem within the team, the number of commits of the team's developers in the project should be increased."
         self.assertEqual(explainability_cadocs_ui,message_example_UI_explainability)
         
- #function to test the incorrect representation of explainability of community smells ui       
+ #function to test the incorrect representation of explainability of community smells UI       
  def test_case_9_1_CR_2_ui(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -188,7 +188,7 @@ class TestSuiteCR2(unittest.TestCase):
 
 
 
-  #function to test the correct representation of explainability of community smells tc       
+  #function to test the correct representation of explainability of community smells TC      
  def test_case_10_CR_2_tc(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
@@ -197,7 +197,7 @@ class TestSuiteCR2(unittest.TestCase):
         message_example_TC_explainability="From the analysis implemented on Explainability using the SHAP framework, we can deduce that the prediction of the Community Smells TC, depends on the socio-technical metric NPR, the total number of pull requests. In order to solve the problem within the team, the number of pull requests of the team developers in the project should be increased."
         self.assertEqual(explainability_cadocs_tc,message_example_TC_explainability)
         
-  #function to test the incorrect representation of explainability of community smells tc       
+  #function to test the incorrect representation of explainability of community smells TC     
  def test_case_10_1_CR_2_tc(self):
      with open('src/community_smells.json') as fp:
         data = json.load(fp)
